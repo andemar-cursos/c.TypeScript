0490818bf6f8de92b0fb0b582af1e8de604e1ecc
andemar <andemartinez98@hotmail.com>
2019-11-21 11:03:41 -0500

    Angular firebase

---
+++ typescript-platzi/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDXciKiSw8T9Z1Cz-OKL-pjvicQkgenNl0',
+    authDomain: 'typescripts-platzi.firebaseapp.com',
+    databaseURL: 'https://typescripts-platzi.firebaseio.com',
+    projectId: 'typescripts-platzi',
+    storageBucket: 'typescripts-platzi.appspot.com',
+    messagingSenderId: '1090487406273',
+    appId: '1:1090487406273:web:4e64fe6ad5c0da1c768e40',
+    measurementId: 'G-SFDJYJYGP4'
+  }
+};
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
